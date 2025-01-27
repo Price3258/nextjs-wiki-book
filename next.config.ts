ee1004710f950b8a2bed6966c7cbf4e0c9e40138
김원태 <rladnjsxo123@naver.com>
2025-01-27 22:15:53 +0900

    chore: config setup

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,22 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let comilerConfig = {};
+    if (process.env.NODE_ENV === 'production') {
+      comilerConfig = {
+        // production 환경에서는 리액트 테스팅 라이브러리에서 사용하는 data-testid 속성을 제거합니다.
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      };
+    }
+
+    comilerConfig = {
+      ...comilerConfig,
+      styeldComponents: true,
+    };
+
+    return comilerConfig;
+  })(),
 };
 
 export default nextConfig;
