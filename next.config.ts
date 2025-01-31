15fe8666a32af83808565db0367d73f601f51a25
김원태 <rladnjsxo123@naver.com>
2025-01-31 20:26:26 +0900

    chore: set next config

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,16 @@ const nextConfig: NextConfig = {
 
     return comilerConfig;
   })(),
+  async rewrites() {
+    return [
+      {
+        // ex. /api/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        // ex. http://localhost:8000
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
