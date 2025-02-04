898145a70d4cff401f37e62d6a681d3e3f85596d
김원태 <rladnjsxo123@naver.com>
2025-02-04 21:24:42 +0900

    fix: anchor 제거 a in a 버그

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -26,17 +26,17 @@ const Footer = () => {
           <nav>
             <Box marginBottom={2}>
               <Link href="/" passHref>
-                <Anchor as="a">톱</Anchor>
+                <Anchor>톱</Anchor>
               </Link>
             </Box>
             <Box marginBottom={2}>
               <Link href="/" passHref>
-                <Anchor as="a">채용</Anchor>
+                <Anchor>채용</Anchor>
               </Link>
             </Box>
             <Box marginBottom={2}>
               <Link href="/" passHref>
-                <Anchor as="a">알림</Anchor>
+                <Anchor>알림</Anchor>
               </Link>
             </Box>
           </nav>
@@ -48,30 +48,29 @@ const Footer = () => {
           <nav>
             <Box marginBottom={2}>
               <Link href="/" passHref>
-                <Anchor as="a">사용 규약</Anchor>
+                <Anchor>사용 규약</Anchor>
               </Link>
             </Box>
             <Box marginBottom={2}>
               <Link href="/" passHref>
-                <Anchor as="a">개인 정보 정책</Anchor>
+                <Anchor>개인 정보 정책</Anchor>
               </Link>
             </Box>
             <Box marginBottom={2}>
               <Link href="/" passHref>
-                <Anchor as="a">배송 및 반품</Anchor>
+                <Anchor>배송 및 반품</Anchor>
               </Link>
             </Box>
           </nav>
         </Box>
         <Box minWidth={{ base: '100%', md: '120px' }}>
           <nav>
-            <Anchor
-              as="a"
+            <Link
               href="https://github.com/moseskim/ts-nextbook-app"
               target="_blank"
             >
               <GitHubIcon size={22} />
-            </Anchor>
+            </Link>
           </nav>
         </Box>
       </Flex>
